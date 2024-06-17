8b2d7953e6bf806ef5c7359c791fccfbbf36fabe
adomasmb <136833767+adomasmb@users.noreply.github.com>
2024-06-17 15:40:52 +0300

    Update About.svelte

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -28,28 +28,30 @@
 <div class="text-white">
 	<h1 id="about" bind:this={section}>About</h1>
 	<p>
-		Adomas Morkūnas-Budrys is one of the most promising Lithuanian conductors of his generation.
+		Adomas Morkūnas-Budrys is one of the most promising Lithuanian conductors of his generation. 
 		He studied with Prof. Johannes Schlaefli at the Zürich University of the Arts and, as an exchange student,
-		at the Norwegian Academy of Music. Adomas is continuing his studies at the Sibelius Academy.
+		at the Norwegian Academy of Music. Adomas is continuing his studies at the Sibelius Academy with Prof. Sakari Oramo.
+
 
 	</p>
 	{#if !mobile || showMore}
 		<div class="max-[600px]:hidden" class:!block={ready} transition:slide={{ duration: slideDuration }}>
 			<br />
 			<p>
-				In the 2023/24 season, Adomas was reinvited to conduct the Lithuanian National Symphony Orchestra and will debut
-				with such orchestras as Kremerata Baltica, Oulu Sinfonia, and Swiss Youth Symphony. As an Assistant conductor and Repetiteur,
-				Adomas will work at the Salzburg Festival in the summer of 2024 on M. Weinberg's opera
-				production "The Idiot". He will also assist Mirga Gražinytė-Tyla with another M. Weinberg's opera, "The Passenger", at Teatro Real in Madrid.
-				As Repetiteur and Assistant Conductor, he will work at the Schleswig-Holsteinisches Landestheater for the production of C. Millöcker's
-				operetta "Der arme Jonathan".
+				In the summer of 2024, Adomas will debut at the Salzburg Festival, where he will work as Assistant conductor and Repetiteur 
+				on M. Weinberg’s opera The Idiot. He will also debut at the Lithuanian National Opera and Ballet Theatre as a Conductor for the 
+				ballet production Le Parc. In the 2023/24 season, Adomas was reinvited to conduct the Lithuanian National Symphony Orchestra 
+				and debuted with such orchestras as Kremerata Baltica, Oulu Sinfonia, and Swiss Youth Symphony. He also assisted Mirga Gražinytė-Tyla 
+				with another M. Weinberg's opera, The Passenger, at Teatro Real in Madrid. As Repetiteur and Assistant Conductor, he worked 
+				at the Schleswig-Holsteinisches Landestheater for the production of C. Millöcker's operetta Der arme Jonathan.
+
 			</p>
 			<br />
 			<p>
-				In the last few years, Adomas appeared with orchestras such as the Finnish Radio Symphony, Helsinki Philharmonic, Manchester Camerata,
-				Lithuanian National Symphony, Lithuanian Chamber, Thessaloniki State Symphony, and Südwestdeutsche Philharmonie Konstanz.
-				As Assistant conductor, he has also worked in opera houses such as Bayerische Staatsoper and Opera de Lyon. Adomas has received valuable
-				advice from such maestros as Paavo Järvi, Bernard Haitink, Jorma Panula, Nicolas Pasquet, Larry Rachleff, and others.
+				In the last few years, on different occasions, Adomas appeared with orchestras such as the Finnish Radio Symphony, Helsinki Philharmonic,
+				Lithuanian National Symphony, Lithuanian Chamber, Thessaloniki State Symphony, and others. As Assistant conductor, he has also worked in
+				opera houses such as Bayerische Staatsoper and Opera de Lyon. Adomas has received valuable advice from such maestros as Paavo Järvi, 
+				Bernard Haitink, Jorma Panula, Nicolas Pasquet, Larry Rachleff, and others.
 			</p>
 			<br />
 			<p>
