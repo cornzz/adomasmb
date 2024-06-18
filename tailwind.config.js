11f13aa720de5be27b5b01750eab364ff5f51acd
cornzz <39997278+cornzz@users.noreply.github.com>
2024-06-18 18:17:01 +0200

    New landing background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
 				dark: '#0d0f12'
 			},
 			backgroundImage: {
-				'landing-image': 'url("/images/adomas-mb-cover.webp")'
+				'landing-image': 'url("/images/adomas-mb-cover.jpg")'
 			},
 			boxShadow: {
 				input: '0 0 0px 2px theme("colors.primary")'
