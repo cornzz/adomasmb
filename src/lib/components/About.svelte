938d3abd916b125ec65f44e331bda3e510983f32
adomasmb <136833767+adomasmb@users.noreply.github.com>
2024-10-07 23:33:34 +0300

    Update About.svelte

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -28,34 +28,25 @@
 <div class="text-white">
 	<h1 id="about" bind:this={section}>About</h1>
 	<p>
-		Lithuanian conductor Adomas Morkūnas-Budrys recently made his debut at the renowned Salzburg Festival, serving as Assistant Conductor 
-		and Repetiteur for M. Weinberg’s The Idiot, as well as assisting in the production of The Passenger at Teatro Real in Madrid. His celebrated debut 
-		at the Lithuanian National Opera, conducting the ballet Le Parc, further demonstrated his versatility and command 
-		on the podium. Throughout the 2023/24 season, Adomas continued to make his mark, leading ensembles such as Kremerata Baltica,
-		Oulu Sinfonia, Swiss Youth Symphony, and the Lithuanian National Symphony Orchestra.
+		Adomas Morkūnas-Budrys is a Lithuanian conductor who recently debuted at the prestigious Salzburg Festival, serving as Assistant Conductor and Repetiteur for M. Weinberg’s <i>The Idiot</i>. He also assisted in another production of Weinberg’s operas, <i>The Passenger</i>, at Teatro Real in Madrid and C. Millöcker’s operetta <i>Der arme Jonathan</i> at <i>Schleswig-Holschteinisches Landestheater</i>. Throughout the 2023/24 season, Adomas continued to make his mark, leading ensembles such as Kremerata Baltica, Oulu Sinfonia, Swiss Youth Symphony, and the Lithuanian National Symphony Orchestra.
 	</p>
 	{#if !mobile || showMore}
 		<div class="max-[600px]:hidden" class:!block={ready} transition:slide={{ duration: slideDuration }}>
 			<br />
 			<p>
-				In recent years, Adomas has stood in front of distinguished orchestras, including the Finnish Radio Symphony, Helsinki Philharmonic,
-				Manchester Camerata, Lithuanian Chamber Orchestra, and Thessaloniki State Symphony, among others. As a part of the musical team, 
-				he has collaborated with major opera houses such as Bayerische Staatsoper and Opéra de Lyon. He has also had the privilege of receiving 
-				mentorship from renowned conductors like Paavo Järvi, Bernard Haitink, Jorma Panula, Nicolas Pasquet, and Larry Rachleff.
+				In recent years, Adomas has stood in front of distinguished orchestras, including the Finnish Radio Symphony, Helsinki Philharmonic, Manchester Camerata, Lithuanian Chamber Orchestra, and Thessaloniki State Symphony. Being part of the musical team for G. F. Haas's opera <i>Bluthaus</i>, he has collaborated with major opera houses such as <i>Bayerische Staatsoper</i> and <i>Opéra de Lyon</i>. He has also had the privilege of receiving mentorship from renowned conductors like Paavo Järvi, Bernard Haitink, Jorma Panula, Nicolas Pasquet, and Larry Rachleff.
 			</p>
 			<br />
 			<p>
-				Adomas is also an avid language learner, a passionate basketball and poker player, and an enthusiastic explorer of XX-XXI Eastern 
-				European music, focusing on discovering and promoting little-known masterpieces. Beyond conducting, Adomas is an accomplished pianist, 
-				arranger, and organizer of musical events. Since 2018, he has been the driving force behind the Future Symphony Competition, which has 
-				been held three times. As Conductor of the Future Symphony Orchestra, Adomas has premiered 19 new orchestral compositions, demonstrating 
-				his commitment to fostering contemporary music.
+				His musical journey has been shaped by his studies under Prof. Johannes Schlaefli at the Zürich University of the Arts and further expanded through an exchange program at the Norwegian Academy of Music. Currently, he is advancing his studies at the prestigious Sibelius Academy under the guidance of Prof. Sakari Oramo.
 			</p>
 			<br />
 			<p>
-				His musical journey has been shaped by his studies under Prof. Johannes Schlaefli at the Zürich University of the Arts and further 
-				expanded through an exchange program at the Norwegian Academy of Music. Currently, he is advancing his studies at the prestigious 
-				Sibelius Academy under the guidance of Prof. Sakari Oramo
+				Adomas is an enthusiastic explorer of XX-XXI Eastern European music, focusing on discovering and promoting little-known masterpieces. Beyond conducting, Adomas is an accomplished pianist, arranger, and organizer of musical events. Since 2018, he has been the driving force behind the Future Symphony Competition, held three times. As Conductor of the Future Symphony Orchestra, Adomas has premiered 19 new orchestral compositions, demonstrating his commitment to fostering contemporary music.
+			</p>
+			<br />
+			<p>
+				Apart from music, Adomas is also an avid language learner, a passionate basketball player, a poker enthusiast, and a soon-to-be pilot.
 			</p>
 		</div>
 	{/if}
