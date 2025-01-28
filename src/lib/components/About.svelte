ead64edcdae351d821b0898092dcd749ae56ba52
adomasmb <136833767+adomasmb@users.noreply.github.com>
2025-01-28 17:50:27 +0200

    Update About.svelte

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -28,17 +28,17 @@
 <div class="text-white">
 	<h1 id="about" bind:this={section}>About</h1>
 	<p>
-		Adomas Morkūnas-Budrys is a Lithuanian conductor who recently debuted at the prestigious Salzburg Festival, serving as Assistant Conductor and Repetiteur for M. Weinberg’s <i>The Idiot</i>. Throughout the 2023/24 season, Adomas continued to make his mark, leading ensembles such as Kremerata Baltica, Oulu Sinfonia, Swiss Youth Symphony, and the Lithuanian National Symphony Orchestra, as well as working in such opera theatres as <i>Teatro Real</i> (on M. Weinberg's opera <i>The Passenger</i>) and <i>Schleswig-Holschteinisches Landestheater</i> (on C. Millöcker’s operetta <i>Der arme Jonathan</i>).
+		Adomas Morkūnas-Budrys is a talented Lithuanian conductor currently pursuing advanced studies at the prestigious Sibelius Academy. He previously earned his Bachelor's degree from the Zürich University of the Arts under the mentorship of Prof. Johannes Schlaefli, with an enriching Erasmus experience at the Norwegian Academy of Music under Prof. Ole Kristian Ruud.
 	</p>
 	{#if !mobile || showMore}
 		<div class="max-[600px]:hidden" class:!block={ready} transition:slide={{ duration: slideDuration }}>
 			<br />
 			<p>
-				In recent years, Adomas has stood in front of distinguished orchestras, including the Finnish Radio Symphony, Helsinki Philharmonic, Manchester Camerata, Lithuanian Chamber Orchestra, and Thessaloniki State Symphony. Being part of the musical team for G. F. Haas's opera <i>Bluthaus</i>, he has collaborated with major opera houses such as <i>Bayerische Staatsoper</i> and <i>Opéra de Lyon</i>. He has also had the privilege of receiving mentorship from renowned conductors like Paavo Järvi, Bernard Haitink, Jorma Panula, Nicolas Pasquet, and Larry Rachleff.
+				In recent years, Adomas has built a remarkable portfolio of engagements with leading orchestras and institutions. His recent highlights include performances with the Lahti Symphony, Jyväskylä Symphony, and the Lithuanian National Opera and Ballet Theatre orchestras, as well as serving as assistant conductor and répétiteur at major venues such as the Salzburg Festival and Teatro Real in Madrid. The 2023/24 season marked further milestones as Adomas conducted ensembles like Kremerata Baltica, Oulu Sinfonia, the Swiss Youth Symphony, and the Lithuanian National Symphony orchestras, in addition to working with the [i]Schleswig-Holsteinisches Landestheater[/i] on Carl Millöcker’s operetta [i]Der arme Jonathan[/i].
 			</p>
 			<br />
 			<p>
-				His musical journey has been shaped by his studies under Prof. Johannes Schlaefli at the Zürich University of the Arts and further expanded through an exchange program at the Norwegian Academy of Music. Currently, he is advancing his studies at the prestigious Sibelius Academy under the guidance of Prof. Sakari Oramo.
+				Adomas’s collaborations extend to many esteemed orchestras, including the Finnish Radio Symphony Orchestra, Helsinki Philharmonic, Manchester Camerata, Lithuanian Chamber Orchestra, and Thessaloniki State Symphony Orchestra. His operatic experience includes being part of the creative team for Georg Friedrich Haas’s opera Bluthaus, working with prominent opera houses like the Bayerische Staatsoper and Opéra de Lyon.
 			</p>
 			<br />
 			<p>
